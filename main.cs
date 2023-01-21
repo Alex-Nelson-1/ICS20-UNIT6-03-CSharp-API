2b3c3b106269dfac7517b9272c997468c254a244
Alex-Nelson-1 <113055690+Alex-Nelson-1@users.noreply.github.com>
2023-01-21 12:00:42 -0500

    Update main.cs

--- main.cs
+++ main.cs
@@ -6,29 +6,26 @@ using System.Net.Http;
 using System.Text.Json;
 using System.Text.Json.Nodes;
 
-class Program {
+class Program
+{
     public static async Task Main()
     {
         HttpClient client = new HttpClient();
         string response = await client.GetStringAsync(
             "https://api.openweathermap.org/data/2.5/weather?lat=45.4211435&lon=-75.6900574&appid=fe1d80e1e103cff8c6afd190cad23fa5"
         );
-
-        
         // Console.WriteLine(response);
-        var jsonAsDictionary  = System.Text.Json.JsonSerializer.Deserialize<Object>(response);
+        var jsonAsDictionary = System.Text.Json.JsonSerializer.Deserialize<Object>(response);
         // Console.WriteLine(jsonAsDictionary);
         Console.WriteLine("");
         JsonNode forecastNode = JsonNode.Parse(response)!;
-
         Console.WriteLine(response);
-        
         // Important Nodes
         JsonNode mainNode = forecastNode!["main"]!;
         JsonNode tempNode = mainNode!["temp"]!;
         JsonNode humidityNode = mainNode!["humidity"]!;
-         JsonNode minNode = mainNode!["temp_min"]!;
-         JsonNode maxNode = mainNode!["temp_max"]!;
+        JsonNode minNode = mainNode!["temp_min"]!;
+        JsonNode maxNode = mainNode!["temp_max"]!;
 
         //Output of important nodes
         Console.WriteLine("\nTemperature: " + tempNode + " K");
